72a5d7680f696e1be4438bed61691d0f4dea1b26
cherethod <sydig.reus@gmail.com>
2025-12-31 20:42:20 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <header>
+
+     </header>
+
+     <main>
+     <aside>
+
+     </aside>
+      <div className='sidebar'>
+        <p>Left SideBar</p>
+      </div>
+      <div className='content'>
+        <p>Main Content</p>
+      </div>
+      <div className='sidebar'>
+        <p>Right SideBar</p>
+      </div>
+     </main>
+
+     <footer>
+
+     </footer>
+    </>
+  )
+}
+
+export default App
